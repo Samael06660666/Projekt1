848ee7cb04e2ecf7a60608773a5246608261189a
Aleksander Wisniewski <samael66606660@gmail.com>
2023-12-03 23:37:23 +0000

    Zadanie domowe dzień4 wersja 2.0

---
+++ Samael06660666/zadanie domowe dzień4/Zdanie domowe dzień 4 wersja 2.0/Zdanie domowe dzień 4 wersja 2.0/Program.cs
@@ -0,0 +1,22 @@
+﻿Console.WriteLine("Napisz Imię:");
+var name = Console.ReadLine();
+
+Console.WriteLine("Napisz płeć");
+var sex = Console.ReadLine();
+
+Console.WriteLine("Napisz wiek");
+var ageasString = Console.ReadLine();
+var age = int.Parse(ageasString);
+
+if (age >= 18)
+{
+    Console.WriteLine("Powyżej 18 roku życia");
+}
+ if (age == 33)
+{
+    Console.WriteLine("Osoba miejąca 33 lata");
+}
+if (age < 18)
+{
+    Console.WriteLine("Osoba niepełnoletnia");
+}
